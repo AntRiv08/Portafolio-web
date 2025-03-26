fd54bfb0ea2daf074bc35657034f1b92b6afd3cb
Antonio Rivera <antonioo.rivera24@hotmail.com>
2025-03-26 16:54:34 -0500

    nuevos contactos

--- src/components/ContactMe/ContactMe.jsx
+++ src/components/ContactMe/ContactMe.jsx
@@ -7,16 +7,26 @@ export const ContactMe = () => {
     <section className='contact-container' id='contact'>
       <h5>Contactos</h5>
       <div className='contact-content'>
-        <div style={{ flex: 1 }}>
+        <div style={{ flex: 1 }} className='contact-info-grid'>
           <ContactInfoCard
             iconUlr='./assets/mail.webp'
             text='👉 Enviame un correo'
             link='mailto:antonioo.rivera24@hotmail.com'
           />
           <ContactInfoCard
+            iconUlr='./assets/whatsapp.webp'
+            text='👉 Mi WhatsApp'
+            link='https://wa.me/593962292245'
+          />
+          <ContactInfoCard
             iconUlr='./assets/linkedin.webp'
             text='👉 Mi LinkedIn'
             link='https://www.linkedin.com/in/antonio-rivera-ibarra-1b93b6269'
+          />
+          <ContactInfoCard
+            iconUlr='./assets/github.webp'
+            text='👉 Mi GitHub'
+            link='https://github.com/AntRiv08'
           />
         </div>
         <div style={{ flex: 1 }}>
